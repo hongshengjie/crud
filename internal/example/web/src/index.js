16e3eb092dd012aa9b5eb94f07d06e7152c57463
hongshengjie <hongshengjie@live.com>
2022-02-24 19:02:38 +0800

    web can run

--- internal/example/web/src/index.js
+++ internal/example/web/src/index.js
@@ -5,7 +5,7 @@
 
    // Lodash, currently included via a script, is required for this line to work
    // Lodash, now imported by this script
-    element.innerHTML = _.join(['Hello', 'webpack'], ' ');
+    element.innerHTML = _.join(['Hello', 'Hongshengjie'], ' ');
 
     return element;
   }
