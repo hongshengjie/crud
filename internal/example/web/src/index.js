9d97bd0e4e10fbad2aa32b57b548bb2dba829369
hongshengjie <hongshengjie@live.com>
2022-02-24 20:10:10 +0800

    update

--- internal/example/web/src/index.js
+++ internal/example/web/src/index.js
@@ -1,4 +1,16 @@
  import _ from 'lodash';
+ const {User, UserId} = require('../proto/user.api_pb.js');
+ const {UserServiceClient} = require('../proto/user.api_grpc_web_pb.js');
+
+ var client = new UserServiceClient('http://localhost:8080');
+
+ var request;
+ request = new UserId();
+ request.setId(1);
+
+ client.getUser(request, {}, (err, response) => {
+   console.log(response.toObject());
+ });
 
   function component() {
     var element = document.createElement('div');
